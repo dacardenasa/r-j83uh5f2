48a67137e96bebd1411d6ef90c71140326f3c4f1
dacardenasa <diego.cardenasaleg@gmail.com>
2020-06-24 17:28:31 -0500

    Add counter chars function to component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.getLength = this.getLength.bind(this);
+
+    this.state = {
+      counter: 0
+    }
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.getLength}></textarea>
+        <div className="counter">{this.state.counter}</div>
       </div>
     );
+  }
+
+  getLength(event){
+    this.setState({
+      counter: event.target.value.length
+    });
   }
 }
 
